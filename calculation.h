0cdc37c1dde72380346654c4bc692f1c677fa2aa
Jae Won Choi <choijaewon959@gmail.com>
2026-03-30 16:18:11 -0500

    update calculation.h

--- calculation.h
+++ calculation.h
@@ -14,5 +14,4 @@ void multiply_mm_naive(const double* matrixA, int rowsA, int colsA, const double
 void multiply_mm_transposed_b(const double* matrixA, int rowsA, int colsA, const double* matrixB_transposed, int rowsB, int colsB, double* result);
 
 
-
 #endif //ADVANCED_COMPUTING_CALCULATION_H
