98e454843d6c68150602a63e26ce7e6dea5b8123
Mohammed RHAZI <simorhazi@Mohammeds-MacBook-Air.local>
2026-03-30 16:14:02 -0500

    Add main, Cmake setup and calculation updates

--- calculation.cpp
+++ calculation.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "calculation.h"
+void multiply_mv_row_major(const double* matrix, int rows, int cols, const double* vector, double* result) {
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<cols; j++) {
+            int idx = i*cols + j;
+            result[idx] = matrix[idx] * vector[j];
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Mohammed RHAZI on 30/03/2026.
+//
+int main() {
+    return 0;
+}
