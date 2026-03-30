6fad6dd696b7ef8a0f480edb6519f6b801c0f33a
Mohammed RHAZI <simorhazi@Mohammeds-MacBook-Air.local>
2026-03-30 16:21:04 -0500

    update multiply_mv_row_major

--- calculation.cpp
+++ calculation.cpp
@@ -5,9 +5,10 @@
 #include "calculation.h"
 void multiply_mv_row_major(const double* matrix, int rows, int cols, const double* vector, double* result) {
     for (int i=0; i<rows; i++) {
+        double sum=0.0;
         for (int j=0; j<cols; j++) {
-            int idx = i*cols + j;
-            result[idx] = matrix[idx] * vector[j];
+            sum += matrix[i*cols + j] * vector[j];
         }
+        result[i]=sum;
     }
 }
