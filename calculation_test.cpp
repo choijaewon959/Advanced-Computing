dfd6f03e5f214602c9ec9089cbe777f99f283eb3
Mohammed RHAZI <simorhazi@Mohammeds-MacBook-Air.local>
2026-03-30 19:43:50 -0500

    update test_multiply_mv_col_major

--- calculation_test.cpp
+++ calculation_test.cpp
@@ -11,33 +11,54 @@ void test_multiply_mv_row_major() {
     int colsA = 3;
 
     double* A = new double[rowsA * colsA];
-    double* V = new double[colsA];
+    double* vec = new double[colsA];
     double* result = new double[rowsA];
 
     double A_vals[] = {1,2,3,4,5,6};
     double V_vals[] = {1,2,3};
 
-    for (int i =0; i<rowsA * colsA; ++i) {
-        A[i]=A_vals[i];
-    }
-    for (int i=0; i<colsA; ++i) {
-        V[i]=V_vals[i];
-    }
-    for (int i=0; i<rowsA; ++i) {
-        result[i]=0.0;
-    }
+    std::copy(A_vals, A_vals + rowsA*colsA, A);
+    std::copy(V_vals, V_vals + colsA, vec);
 
-    multiply_mv_row_major(A, rowsA, colsA, V, result);
+    multiply_mv_row_major(A, rowsA, colsA, vec, result);
     double expected[] = {14,32};
     for (int i = 0; i<rowsA; ++i) {
-        assert(result[i] == expected[i]);
+        assert(std::abs(result[i] - expected[i]) < 1e-9);
     }
 
     delete[] A;
-    delete[] V;
+    delete[] vec;
     delete[] result;
 
-    std::cout << "test_test_multiply_mv_row_major_passed ...\n";
+    std::cout << "test_multiply_mv_row_major_passed ...\n";
+}
+
+void test_multiply_mv_col_major() {
+    int rowsA = 2;
+    int colsA = 3;
+
+    double* A = new double[rowsA * colsA];
+    double* vec = new double[colsA];
+    double* result = new double[rowsA];
+
+    double A_vals[] = {1,2,3,4,5,6};
+    double vec_vals[] = {1,2,3};
+
+    std::copy(A_vals, A_vals + rowsA*colsA, A);
+    std::copy(vec_vals, vec+colsA, vec);
+    multiply_mv_col_major(A, rowsA, colsA, vec, result);
+
+    double expected[] = {22, 28};
+
+    for (int i=0; i<rowsA; ++i) {
+        assert(abs(result[i] - expected[i]) < 1e-9);
+    }
+
+    delete[] A;
+    delete[] vec;
+    delete[] result;
+
+    std::cout << "test_multiply_mv_col_major passed ... \n";
 }
 
 void test_multiply_mm_naive() {
@@ -124,6 +145,7 @@ void test_multiply_mm_transposed_b_basic() {
 
 int main() {
     test_multiply_mv_row_major();
+    test_multiply_mv_col_major();
     test_multiply_mm_naive();
     test_multiply_mm_transposed_b_basic();
 
--- main.cpp
+++ main.cpp
@@ -7,16 +7,4 @@
 #include "calculation.h"
 int main() {
 
-
-
-    //tests
-    double matrix[6] = {1,2,3,4,5,6};
-    double vec[3] = {1,2,3};
-    double res[2];
-
-    multiply_mv_row_major(matrix, 2,3,vec, res);
-    std::cout << res[0] << " " << res[1] << std::endl;
-
-    multiply_mv_col_major(matrix, 2,3,vec, res);
-    std::cout << res[0] << " " << res[1] << std::endl;
 }
