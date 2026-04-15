031488b418e2d28444c7fa503d865f3ea0eca755
Jae Won Choi <choijaewon959@gmail.com>
2026-04-15 18:48:44 -0500

    adding hw3

---
+++ HW3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+struct Trade {
+    std::string symbol;
+    double price;
+
+    Trade(const std::string& sym, double p)
+        : symbol(sym), price(p) {}
+};
+
+int main() {
+
+}
