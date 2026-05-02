2a587164a2a00c0511e30cf9a96321622d1b5139
Mohammed RHAZI <simorhazi@Mohammeds-MacBook-Air.local>
2026-05-02 15:20:09 -0500

    implement orderbook

--- Phase5/OrderBook.h
+++ Phase5/OrderBook.h
@@ -4,5 +4,44 @@
 
 #ifndef PHASE5_ORDERBOOK_H
 #define PHASE5_ORDERBOOK_H
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+struct Order {
+    std::string id;
+    double price;
+    int quantity;
+    bool isBuy;
+};
+
+class OrderBook {
+    std::map<double, std::unordered_map<std::string, Order>> orderLevels;
+    std::unordered_map<std::string, Order> orderLookup;
+
+public:
+    void addOrder(const std::string& id, double price, int quantity, bool isBuy) {
+        Order order{id, price, quantity, isBuy};
+        orderLevels[price][id] = order;
+        orderLookup[id] = order;
+    }
+    void deleteOrder(const std::string& id) {
+        auto it = orderLookup.find(id);
+        if (it != orderLookup.end()) {
+            orderLevels[it->second.price].erase(id);
+            orderLookup.erase(it);
+        }
+    }
+    void modifyOrder(const std::string& id, double pr, double qty) {
+        auto it = orderLookup.find(id);
+        if (it != orderLookup.end()) {
+            orderLevels[it->second.price].erase(id);
+            addOrder(id, pr, qty, it->second.isBuy);
+        }
+    }
+};
+
+
 
 #endif //PHASE5_ORDERBOOK_H
